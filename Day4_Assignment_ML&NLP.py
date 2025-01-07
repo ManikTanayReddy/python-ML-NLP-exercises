19f95bd41e24e395becc9c6bd2339515e0667425
ManikTanayReddy <153349043+ManikTanayReddy@users.noreply.github.com>
2025-01-07 12:04:39 +0530

    Update Day4_Assignment_ML&NLP.py

--- Day4_Assignment_ML&NLP.py
+++ Day4_Assignment_ML&NLP.py
@@ -1,5 +1,4 @@
-Q1. Write a Python program to calculate the sum of all even numbers between 1 and a given positive integer n
-
+#Q1. Write a Python program to calculate the sum of all even numbers between 1 and a given positive integer n
 
 def sum_of_even_integers(n):
     sum=0
@@ -8,6 +7,6 @@ def sum_of_even_integers(n):
             sum+=i
     return sum
 
-n=int(input("Enter a positive integer to calculate even number till the number "))   
+n=int(input("Enter a positive integer to calculate even numberS till the given number "))   
 result=sum_of_even_integers(n)
-print(f"Sum of even number till {n} values is ",result)
+print(f"Sum of even number till {n}  is ",result)
